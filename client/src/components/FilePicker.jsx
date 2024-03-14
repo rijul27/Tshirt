b23d55a4240a59e7d26997552651abf55c742e44
rijul27 <122171989+rijul27@users.noreply.github.com>
2024-03-14 14:48:17 +0530

    FilePicker.jsx

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -13,7 +13,7 @@ const FilePicker = ({ file, setFile, readFile }) => {
           onChange={(e) => setFile(e.target.files[0])}
         />
         <label htmlFor="file-upload" className="filepicker-label">
-          Upload File
+          Upload File for custom Logo
         </label>
 
         <p className="mt-2 text-gray-500 text-xs truncate">
